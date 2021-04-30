6c3e45a077421a94494bf460033351350e4ee11e
foraphe <foraphe@gmail.com>
2021-05-01 00:11:50 +0800

    minor fixes

--- src/extendCommands/choices.js
+++ src/extendCommands/choices.js
@@ -1,7 +1,7 @@
 function run(context) {
     const reg = \u8fd8\u662f;
     // "还是"
-    const choices = context.message.substr(1, context.message.length).replace(reg, ' ');
+    const choices = context.message.substr(1, context.message.length).replace(reg, ' ').split(' ');
     if (choices.length <= 1) return;
     const randInt = parseInt(Math.random() * (choices.length - 1));
     return choices[randInt];
