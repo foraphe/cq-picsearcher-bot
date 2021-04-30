5518a712666fb80c2fd346bc8a0c5a0ea0a0f833
foraphe <foraphe@gmail.com>
2021-05-01 00:14:07 +0800

    minor fixes

--- src/extendCommands/choices.js
+++ src/extendCommands/choices.js
@@ -1,5 +1,5 @@
 function run(context) {
-    const reg = \u8fd8\u662f;
+    const reg = '\u8fd8\u662f';
     // "还是"
     const choices = context.message.substr(1, context.message.length).replace(reg, ' ').split(' ');
     if (choices.length <= 1) return;
