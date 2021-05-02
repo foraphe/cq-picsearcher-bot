a7f93df1a3fae28e22e0944d5e77086fbec363eb
foraphe <foraphe@gmail.com>
2021-05-02 11:05:45 +0800

    extendCommand refactor

---
+++ src/extendCommands/extendCommands.js
@@ -0,0 +1,26 @@
+const commands = [
+    {
+        // "A是不是B" | "A还是B"
+        "listen": (context) => context.message.indexOf('\u8fd8\u662f|\u662f\u4e0d\u662f') !== -1,
+        "module": "src/extendCommands/choices",
+        "exec": (module, context, config) => module(context, config),
+        "config": "choice"
+    },
+    {
+        // "A不AB"
+        "listen": (context) => {
+            if (context.message.indexOf('\u4e0d') !== -1) {
+                const spl = context.message.indexOf('\u4e0d');
+                const prefix = context.message.substr(1, spl - 1);
+                if (context.message.substr(spl + 1, prefix.length) === prefix) {
+                    return true;
+                }
+            }
+        },
+        "exec": (module, context, config) => module(context, config),
+        "module": "src/extendCommands/choices",
+        "config": "choice"
+    }
+];
+
+module.exports = commands;
