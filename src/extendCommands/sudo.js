559ef3f28db21b6ee62ed9b218efd4d500c7a93e
foraphe <foraphe@gmail.com>
2021-05-25 18:45:15 +0800

    sudo.js

--- src/extendCommands/sudo.js
+++ src/extendCommands/sudo.js
@@ -6,7 +6,7 @@ function run(context, config) {
     const command = context.message.substr(6);
     switch (command) {
         case 'load':
-            const retv = `mem: ${parseInt(os.freemem / 1048576)}MB/${parseInt(os.totalmem / 1048576)}MB\nload: ${os.loadavg()}`;
+            const retv = `mem: ${parseInt(os.freemem / 1048576)}MB/${parseInt(os.totalmem / 1048576)}MB\nload: ${Math.round(os.loadavg()[0] * 100) / 100},${Math.round(os.loadavg()[2] * 100) / 100}`;
             global.replyMsg(context, retv);
     }
 }
