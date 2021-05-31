dbe667a723cf06d73dd0c3c33335e70d94dc7b12
foraphe <foraphe@gmail.com>
2021-05-31 18:00:35 +0800

    setu send destination

--- src/extendCommands/sudo.js
+++ src/extendCommands/sudo.js
@@ -3,22 +3,21 @@ const os = require('os');
 function run(context, config) {
     if (!config.enabled) return;
     if (config.admins.indexOf(context.user_id) === -1) return;
-    var tmp = context.message.substr(6);
-    const command = tmp.substr(0, tmp.match(' ').index);
-    switch (command) {
-        case 'load':
-            const retv = `mem: ${parseInt(os.freemem / 1048576)}MB/${parseInt(os.totalmem / 1048576)}MB\nload: ${Math.round(os.loadavg()[0] * 100) / 100},${Math.round(os.loadavg()[2] * 100) / 100}`;
-            global.replyMsg(context, retv);
-            break;
-        case 'setconfig':
-            const conf = tmp.substr(10);
-            if (conf === 'setudest true' || conf === 'setudest false') {
-                if (!global.extendConfig || global.extendConfig.error) break;
-                const boolv = conf.substr(9);
-                global.extendConfig.misc.setu.privateAll = eval(boolv);
-                global.replyMsg(`setu私聊发送已设置为${boolv}`);
-            }
+    const command = context.message.substr(6);
+    if (command === 'load') {
+        const retv = `mem: ${parseInt(os.freemem / 1048576)}MB/${parseInt(os.totalmem / 1048576)}MB\nload: ${Math.round(os.loadavg()[0] * 100) / 100},${Math.round(os.loadavg()[2] * 100) / 100}`;
+        global.replyMsg(context, retv);
     }
+    else if (command.indexOf('setConfig') === 0) {
+        const conf = command.substr(10);
+        if (conf === 'setudest true' || conf === 'setudest false') {
+            if (!global.extendConfig || global.extendConfig.error) return;
+            const boolv = conf.substr(9);
+            global.extendConfig.misc.setu.privateAll = eval(boolv);
+            global.replyMsg(`setu私聊发送已设置为${boolv}`);
+        }
+    }
+}
 }
 
 module.exports = run;
