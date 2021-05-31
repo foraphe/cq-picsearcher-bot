0367dec3e8c4143ba2ccf99eeb2f2061a7d3d660
foraphe <foraphe@gmail.com>
2021-05-31 17:48:12 +0800

    setu send destination

--- src/extendCommands/sudo.js
+++ src/extendCommands/sudo.js
@@ -8,6 +8,15 @@ function run(context, config) {
         case 'load':
             const retv = `mem: ${parseInt(os.freemem / 1048576)}MB/${parseInt(os.totalmem / 1048576)}MB\nload: ${Math.round(os.loadavg()[0] * 100) / 100},${Math.round(os.loadavg()[2] * 100) / 100}`;
             global.replyMsg(context, retv);
+            break;
+        case 'setconfig':
+            const conf = command.substr(10);
+            if (conf === 'setudest true' || conf === 'setudest false') {
+                if (!global.extendConfig || global.extendConfig.error) break;
+                const boolv = conf.substr(9);
+                global.extendConfig.misc.setu.privateAll = eval(boolv);
+                global.replyMsg(`setu私聊发送已设置为${boolv}`);
+            }
     }
 }
 
