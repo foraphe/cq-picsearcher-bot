83631a6abc891e44fcca0959494be48935fa59e1
foraphe <foraphe@gmail.com>
2021-06-26 22:50:16 +0800

    roll.js

---
+++ src/extendCommands/roll.js
@@ -0,0 +1,13 @@
+function randInt(length) {
+    return parseInt(Math.random() * (length));
+}
+
+function run(context, config) {
+    if (!config) return;
+    const range = parseInt(context.message.substr(6));
+    var r = 100;
+    if (range !== NaN && typeof range === 'number') {
+        r = range;
+    }
+    global.replyMsg(context, '' + randInt(r), config.replyWithAt);
+}
