4e27be188db6df74d0565aebbcbd1548d2b5e210
foraphe <foraphe@gmail.com>
2021-08-10 22:03:59 +0800

    bug fixes

--- src/extendCommands/poke.js
+++ src/extendCommands/poke.js
@@ -1,6 +1,6 @@
 function run(context, config) {
     if (!config.enabled || !context.message_type == 'group') return;
-    return global.bot.replyMsg(context,`[CQ:poke,qq=${context.user_id}]`)
+    return global.replyMsg(context,`[CQ:poke,qq=${context.user_id}]`)
 }
 
 module.exports = run;
