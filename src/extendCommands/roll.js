07334955ef80505d8b6f9b6810ff3ccddc8dcfae
foraphe <foraphe@gmail.com>
2021-12-26 11:21:03 +0800

    tmp

--- src/extendCommands/roll.js
+++ src/extendCommands/roll.js
@@ -4,7 +4,6 @@ function randInt(length) {
 
 function run(context, config) {
     if (!config) return;
-    const range = parseInt(context.message.substr(6));
     var r = 100;
     if (range !== NaN && range !== undefined && typeof range === 'number') {
         r = range;
